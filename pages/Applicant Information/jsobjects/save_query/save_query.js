316daf5a1289f378501b80d900a206376cbb8702
rajkumar a a <rajkumaraa@anjaconline.org>
2023-03-16 10:28:07 +0000

    update

---
+++ pages/Applicant Information/jsobjects/save_query/save_query.js
@@ -0,0 +1,27 @@
+export default {
+	loadTableData: () =>{
+		storeValue('dataTable',[{
+			Rounds: 'Round1', Type:"Interviewer" && "Exam",  interviewerremark:'Good',Result:'selected',score:40
+		}])
+	},
+	
+	
+	saveValue: ()=>{
+		let existing_obj = appsmith.store.dataTable;
+		existing_obj.push(rounds.newRow);
+		storeValue('dataTable',existing_obj);
+		console.log(appsmith.store.dataTable);
+	},
+	
+	editValue: async(index, edited_row)=> {
+		let existing_obj = appsmith.store.dataTable;
+		existing_obj[index] = edited_row;
+		storeValue('dataTable',existing_obj);
+	},
+	
+	printValue: () => {
+		console.log(appsmith.store.dataTable);
+	}
+	
+	
+}

0be50c049bb4650bec38a77a711d0f10eaa61820
rajkumar a a <rajkumaraa@anjaconline.org>
2023-03-16 05:52:49 +0000

    System generated commit, initial commit

---
+++ pages/Job/jsobjects/save_queries/save_queries.js
@@ -0,0 +1,27 @@
+export default {
+	loadTableData: () =>{
+		storeValue('dataTable',[{
+			skils: 'nodejs', yearsofexperience: 2,fluency:80
+		}])
+	},
+	
+	
+	saveValue: ()=>{
+		let existing_obj = appsmith.store.dataTable;
+		existing_obj.push(Table1.newRow);
+		storeValue('dataTable',existing_obj);
+		console.log(appsmith.store.dataTable);
+	},
+	
+	editValue: async(index, edited_row)=> {
+		let existing_obj = appsmith.store.dataTable;
+		existing_obj[index] = edited_row;
+		storeValue('dataTable',existing_obj);
+	},
+	
+	printValue: () => {
+		console.log(appsmith.store.dataTable);
+	}
+	
+	
+}
